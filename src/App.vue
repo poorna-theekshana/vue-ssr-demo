335f41dc83046631859250c465af3ef3411697c1
Poorna Theekshana <poornatheekshana@outlook.com>
2023-12-01 13:53:55 +0530

    Full Commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view />
+    <SsrDemo />
+  </div>
+</template>
+
+<script>
+import SsrDemo from './components/SsrDemo.vue';
+
+export default {
+  components: {
+    SsrDemo,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 1.5em;
+  color: #42b983;
+}
+</style>
---
+++ src/components/SsrDemo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="ssr-demo">
+        <h2>Server-Side Rendered Data</h2>
+        <p>{{ serverRenderedData }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            serverRenderedData: 'Data fetched on the server side!',
+        };
+    },
+    mounted() {
+        // Simulate fetching data from an API (you can replace this with actual API calls)
+        // For simplicity, we use a timeout to simulate an asynchronous operation
+        setTimeout(() => {
+            // Update the server-rendered data after fetching
+            this.serverRenderedData = 'Updated data from the server!';
+        }, 2000);
+    },
+};
+</script>
+
+<style scoped>
+.ssr-demo {
+    padding: 20px;
+    margin: 20px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+</style>
